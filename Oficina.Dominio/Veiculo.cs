f7816e561d2d4e8c9b15583ea8ed512caa26fc50
Vitor Neil Avelino <avelino.vitor@gmail.com>
2018-07-28 17:28:58 -0300

    Oficina - HTML

--- Oficina.Dominio/Veiculo.cs
+++ Oficina.Dominio/Veiculo.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Oficina.Dominio
 {
     public class Veiculo
     {
-        public int Id { get; set; }
+        //public Veiculo()
+        //{
+        //    Id = Guid.NewGuid();
+        //}
+
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Placa { get; set; }
         public int Ano { get; set; }
         public string Observacao { get; set; }
--- Oficina.Repositorios.SistemaDeArquivos/CorRepositorio.cs
+++ Oficina.Repositorios.SistemaDeArquivos/CorRepositorio.cs
@@ -3,9 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Oficina.Repositorios.SistemaDeArquivos
 {
@@ -30,6 +27,28 @@ namespace Oficina.Repositorios.SistemaDeArquivos
             }
 
             return cores;
+        }
+
+        public Cor Selecionar(int corId)
+        {
+            Cor cor = null;
+
+            foreach (var linha in File.ReadAllLines(_caminhoArquivo))
+            {
+                var linhaId = Convert.ToInt32(linha.Substring(0, 5));
+
+                if (linhaId == corId)
+                {
+                    cor = new Cor();
+
+                    cor.Id = linhaId;
+                    cor.Nome = linha.Substring(5);
+
+                    break;
+                }
+            }
+
+            return cor;
         }
     }
 }

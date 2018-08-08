acc6059b3b40e1c263bdee420bfc34eaf043d63f
Vítor Neil Avelino <avelino.vitor@gmail.com>
2018-08-08 15:18:47 -0300

    Portfólio

---
+++ AspNetVS2017.Capitulo03.Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetVS2017.Capitulo03.Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetVS2017.Capitulo03.Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Sucesso = true;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContatoViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}

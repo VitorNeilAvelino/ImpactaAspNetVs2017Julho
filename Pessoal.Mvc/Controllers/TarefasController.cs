9abe3f1be4f84c79dfdb9b64e2d269c045e9c109
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-08-26 18:47:07 -0300

    Tarefas com ASP.NET Core

---
+++ Pessoal.Mvc/Controllers/TarefasController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Pessoal.Dominio;
+using Pessoal.Repositorios.SqlServer;
+
+namespace Pessoal.Mvc.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private TarefaRepositorio _tarefaRepositorio;
+
+        public TarefasController(IConfiguration configuration)
+        {
+            _tarefaRepositorio = new TarefaRepositorio(configuration.GetConnectionString("PessoalConnectionString"));
+        }
+
+        // GET: Tarefas
+        public ActionResult Index()
+        {
+            return View(_tarefaRepositorio.Selecionar());
+        }
+
+        // GET: Tarefas/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Tarefas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tarefas/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tarefa tarefa)
+        {
+            try
+            {
+                _tarefaRepositorio.Inserir(tarefa);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError(string.Empty, "Não foi possível inserir a nova tarefa.");
+                return View(tarefa);
+            }
+        }
+
+        // GET: Tarefas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tarefas/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tarefas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tarefas/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
+++ Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
@@ -12,8 +12,17 @@ namespace Pessoal.Repositorios.SqlServer
 {
     public class TarefaRepositorio
     {
-        private string _stringConexao =
-            ConfigurationManager.ConnectionStrings["pessoalConnectionString"].ConnectionString;
+        private string _stringConexao;
+
+        public TarefaRepositorio()
+        {
+            _stringConexao = ConfigurationManager.ConnectionStrings["pessoalConnectionString"].ConnectionString;
+        }
+
+        public TarefaRepositorio(string stringConexao)
+        {
+            _stringConexao = stringConexao;
+        }
 
         public int Inserir(Tarefa tarefa)
         {
@@ -24,6 +33,7 @@ namespace Pessoal.Repositorios.SqlServer
                 using (var comando = new SqlCommand("TarefaInserir", conexao))
                 {
                     comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddRange(Mapear(tarefa));
 
                     return (int)comando.ExecuteScalar();
                 }
@@ -31,6 +41,93 @@ namespace Pessoal.Repositorios.SqlServer
                 //conexao.Close();
             }
 
+        }
+
+        public List<Tarefa> Selecionar()
+        {
+            var tarefas = new List<Tarefa>();
+
+            using (var conexao = new SqlConnection(_stringConexao))
+            {
+                conexao.Open();
+
+                const string nomeProcedure = "TarefaSelecionar";
+
+                using (var comando = new SqlCommand(nomeProcedure, conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        while (registro.Read())
+                        {
+                            tarefas.Add(Mapear(registro));
+                        }
+                    }
+                }
+
+                //conexao.Close();
+            }
+
+            return tarefas;
+        }
+
+        public Tarefa Selecionar(int id)
+        {
+            Tarefa tarefa = null;
+
+            using (var conexao = new SqlConnection(_stringConexao))
+            {
+                conexao.Open();
+
+                const string nomeProcedure = "TarefaSelecionar";
+
+                using (var comando = new SqlCommand(nomeProcedure, conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddWithValue("Id", id);
+
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        if (registro.Read())
+                        {
+                            tarefa = Mapear(registro);
+                        }
+                    }
+                }
+            }
+
+            return tarefa;
+        }
+
+        private Tarefa Mapear(SqlDataReader registro)
+        {
+            var tarefa = new Tarefa();
+
+            tarefa.Id = Convert.ToInt32(registro["Id"]);
+            tarefa.Nome = registro["Nome"].ToString();
+            tarefa.Prioridade = (Prioridade)registro["Prioridade"];
+            tarefa.Concluida = Convert.ToBoolean(registro["Concluida"]);
+            tarefa.Observacoes = Convert.ToString(registro["Observacoes"]);
+
+            return tarefa;
+        }
+
+        private SqlParameter[] Mapear(Tarefa tarefa)
+        {
+            var parametros = new List<SqlParameter>();
+
+            if (tarefa.Id != 0)
+            {
+                parametros.Add(new SqlParameter("@id", tarefa.Id));
+            }
+
+            parametros.Add(new SqlParameter("@nome", tarefa.Nome));
+            parametros.Add(new SqlParameter("@prioridade", tarefa.Prioridade));
+            parametros.Add(new SqlParameter("@concluida", tarefa.Concluida));
+            parametros.Add(new SqlParameter("@Observacoes", tarefa.Observacoes));
+
+            return parametros.ToArray();
         }
     }
 }

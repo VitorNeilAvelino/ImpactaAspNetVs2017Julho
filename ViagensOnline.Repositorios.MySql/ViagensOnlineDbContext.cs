7ce41973b59851290215e75c8b884b5748218b45
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-09-08 19:12:52 -0300

    EF com MySql

---
+++ ViagensOnline.Repositorios.MySql/ViagensOnlineDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.Entity;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.MySql
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class ViagensOnlineDbContext : DbContext
+    {
+        public ViagensOnlineDbContext() : base("ViagensOnlineConnectionString")
+        {
+          
+        }
+
+        public DbSet<Destino> Destinos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ViagensOnline.Repositorios.MySqlTests/ViagensOnlineDbContextTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.MySql.Tests
+{
+    [TestClass()]
+    public class ViagensOnlineDbContextTests
+    {
+        ViagensOnlineDbContext db = new ViagensOnlineDbContext();
+
+        [TestMethod()]
+        public void InserirDestinoTest()
+        {
+            var destino = new Destino();
+
+            destino.Cidade = "São Paulo";
+            destino.Nome = "Conheça São Paulo";
+            destino.NomeImagem = "SP.png";
+            destino.Pais = "Brasil";
+
+            db.Destinos.Add(destino);
+
+            db.SaveChanges();
+        }
+    }
+}
